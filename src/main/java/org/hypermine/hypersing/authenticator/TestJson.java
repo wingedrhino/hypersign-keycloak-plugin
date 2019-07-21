fe776c41bef8869a3fb5fd42ef3649cc13aa6d60
vikramide <vikrambanand.ide@gmail.com>
2019-07-21 11:51:03 +0530

    Forward-fix added got the missing code from feature/HS-AUTH-34

---
+++ src/main/java/org/hypermine/hypersing/authenticator/TestJson.java
@@ -0,0 +1,14 @@
+package org.hypermine.hypersing.authenticator;
+
+import org.json.JSONObject;
+
+public class TestJson {
+
+	public static void main(String[] args) {
+        String response = QRCodeGenerator.getChallenge();
+        // Object o1 = JSONValue.parse(response);
+        JSONObject jsonObject = new JSONObject(response);
+        // System.out.println(jsonObject.data.getString("data"));
+        System.out.println("\nTes JSON : " + jsonObject.toString());
+    }
+}
