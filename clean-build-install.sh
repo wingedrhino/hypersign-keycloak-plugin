bbe2fda8cf074a1b7d8a336e426d72c1f7cbc305
vishwas1 <vishwasanand.cse@gmail.com>
2019-08-17 12:40:43 +0530

    kill keycloack server before runnin

--- clean-build-install.sh
+++ clean-build-install.sh
@@ -19,5 +19,9 @@ cp hs-themes/hypersign.ftl ${KCBASE}/themes/base/login
 echo "Deploying the hypersign plugin.."
 cd ${KCBASE}
 ./bin/jboss-cli.sh --command="module add --name=hanko-plugin-keycloak-ejb --resources=./hanko-plugin-keycloak-ejb-0.2-SNAPSHOT.jar --dependencies=org.keycloak.keycloak-common,org.keycloak.keycloak-core,org.keycloak.keycloak-services,org.keycloak.keycloak-model-jpa,org.keycloak.keycloak-server-spi,org.keycloak.keycloak-server-spi-private,javax.ws.rs.api,javax.persistence.api,org.hibernate,org.javassist,org.liquibase,com.fasterxml.jackson.core.jackson-core,com.fasterxml.jackson.core.jackson-databind,com.fasterxml.jackson.core.jackson-annotations,org.jboss.resteasy.resteasy-jaxrs,org.jboss.logging,org.apache.httpcomponents,org.apache.commons.codec"
+
+echo "Running keycloak.."
+kill -9 $(lsof -t -i:8080)
 ./bin/standalone.sh
+
 echo "Finish!"
