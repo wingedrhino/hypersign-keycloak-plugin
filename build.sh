872b4e7e800e0b979287c1d599d5553ca7c8f846
vishwas1 <vishwasanand.cse@gmail.com>
2019-08-25 15:23:08 +0530

    removed comments from scripts

--- build.sh
+++ build.sh
@@ -11,28 +11,5 @@ echo Building ear package
 mvn clean package
 # mvn clean package -pl \!hanko-plugin-keycloak-ear
 cp hs-plugin-keycloak-ejb/target/hs-plugin-keycloak-ejb-*.jar dist/
+
 exit
-# echo Buildung React frontend
-# cd hanko-account && npm install && npm run build-keycloak
-# cd ..
-
-# echo Copy themes to dist directory
-# cp -r themes dist/themes
-
-# echo Copy index.html
-# mkdir -p dist/themes/base/account
-# cp hanko-account/dist/index.html dist/themes/base/account/account-hanko.ftl
-
-# echo Copy new javascript files
-# cp hanko-account/dist/* dist/themes/keycloak/account/resources/js/
-# cp hanko-account/dist/*.js dist/themes/playground/account/resources/js/
-
-# echo Copy main.css
-# cp hanko-account/dist/main.css dist/themes/keycloak/account/resources/css/main.css
-# mkdir -p  dist/themes/playground/account/resources/css
-# cp hanko-account/dist/playground.css dist/themes/playground/account/resources/css/main.css
-
-# echo Zip new themes
-# cd dist
-# zip -r themes.zip themes
-# cd ..
--- clean-build-install.sh
+++ clean-build-install.sh
@@ -25,7 +25,7 @@ cp hs-themes/hypersign-new.ftl ${KCBASE}/themes/base/login
 
 echo "Deploying the hypersign plugin.."
 cd ${KCBASE}
-./bin/jboss-cli.sh --command="module add --name=hanko-plugin-keycloak-ejb --resources=./hs-plugin-keycloak-ejb-0.2-SNAPSHOT.jar --dependencies=org.keycloak.keycloak-common,org.keycloak.keycloak-core,org.keycloak.keycloak-services,org.keycloak.keycloak-model-jpa,org.keycloak.keycloak-server-spi,org.keycloak.keycloak-server-spi-private,javax.ws.rs.api,javax.persistence.api,org.hibernate,org.javassist,org.liquibase,com.fasterxml.jackson.core.jackson-core,com.fasterxml.jackson.core.jackson-databind,com.fasterxml.jackson.core.jackson-annotations,org.jboss.resteasy.resteasy-jaxrs,org.jboss.logging,org.apache.httpcomponents,org.apache.commons.codec,org.keycloak.keycloak-wildfly-adduser"
+./bin/jboss-cli.sh --command="module add --name=hs-plugin-keycloak-ejb --resources=./hs-plugin-keycloak-ejb-0.2-SNAPSHOT.jar --dependencies=org.keycloak.keycloak-common,org.keycloak.keycloak-core,org.keycloak.keycloak-services,org.keycloak.keycloak-model-jpa,org.keycloak.keycloak-server-spi,org.keycloak.keycloak-server-spi-private,javax.ws.rs.api,javax.persistence.api,org.hibernate,org.javassist,org.liquibase,com.fasterxml.jackson.core.jackson-core,com.fasterxml.jackson.core.jackson-databind,com.fasterxml.jackson.core.jackson-annotations,org.jboss.resteasy.resteasy-jaxrs,org.jboss.logging,org.apache.httpcomponents,org.apache.commons.codec,org.keycloak.keycloak-wildfly-adduser"
 
 echo "Running keycloak.."
 kill -9 $(lsof -t -i:8080)
