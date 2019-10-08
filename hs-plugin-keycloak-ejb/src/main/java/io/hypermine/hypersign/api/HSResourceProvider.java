52df268a54c7a0aacbb33956ea9129f8a5b6165c
vishwas1 <vishwasanand.cse@gmail.com>
2019-10-08 20:45:04 +0530

    added some more hearder

--- hs-plugin-keycloak-ejb/src/main/java/io/hypermine/hypersign/api/HSResourceProvider.java
+++ hs-plugin-keycloak-ejb/src/main/java/io/hypermine/hypersign/api/HSResourceProvider.java
@@ -335,14 +335,22 @@ public class HSResourceProvider implements RealmResourceProvider {
             return Response
             .status(Response.Status.OK)
             .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+            .header("Access-Control-Max-Age", "1209600")
             .entity(respStr)
             .build();            
         }
         catch(Exception e){
             respStr = e.toString();
             return Response
-            .status(Response.Status.BAD_REQUEST)
+            .status(Response.Status.BAD_REQUEST)            
             .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+            .header("Access-Control-Max-Age", "1209600")
             .entity(respStr)
             .build();
         }          
