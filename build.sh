608d8db5353b94935b3afaf3f8544873afa6e9f8
vishwas1 <vishwasanand.cse@gmail.com>
2020-02-04 23:23:25 +0530

    changed the build.sh

--- build.sh
+++ build.sh
@@ -12,4 +12,5 @@ mvn clean package
 cp hs-plugin-keycloak-ejb/target/hs-plugin-keycloak-ejb-*.jar dist/
 tar cvf hs-theme.tar.gz hs-themes
 mv hs-theme.tar.gz dist/ 
+tar cvf hs-authenticator.tar.gz dist/hs-plugin-keycloak-ejb-0.2-SNAPSHOT.jar dist/hs-theme.tar.gz 
 exit
