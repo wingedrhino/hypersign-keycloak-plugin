5a90b0996b680d5f0a609ef2248379e2766e4a62
vishwas1 <vishwasanand.cse@gmail.com>
2020-02-04 22:51:55 +0530

    fixed the build problem

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ mkdir dist
 echo Building ear package
 mvn clean package
 cp hs-plugin-keycloak-ejb/target/hs-plugin-keycloak-ejb-*.jar dist/
-
+tar cvf hs-theme.tar.gz hs-themes
+mv hs-theme.tar.gz dist/ 
 exit
